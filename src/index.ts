13adf31e695d6c8e61f7e582baf49c06a4ac8ebd
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-07-28 07:45:17 -0600

    -- HTTPS CON CERTIFICADO --

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 import https from 'https';
 import fs from 'fs';
 
@@ -25,12 +25,15 @@ dotenv.config();
 
 // Crear la aplicación de Express
 const app: Application = express();
-const port: number = parseInt(process.env.PORT as string, 10) || 3000;
+const port: number = parseInt(process.env.PORT as string, 10) || 8000;
 
 // Middleware de análisis del cuerpo
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+// Middleware para autentificación
+app.use(authMiddleware);
 
 // Rutas de los módulos
 app.use('/api/user', userRoutes);
@@ -40,7 +43,7 @@ app.use('/api/mobiliary', mobiliaryRoutes);
 app.use('/api/client', clientRoutes);
 app.use('/api/supplies', suppliesRoutes);
 app.use('/api/rentedmobiliary', rentedMobiliaryRoutes);
-app.use('/api/packageTypes', packageTypeRoutes)
+app.use('/api/packageTypes', packageTypeRoutes);
 
 // Middleware para manejar rutas no encontradas
 app.use(notFoundHandler);
@@ -48,33 +51,28 @@ app.use(notFoundHandler);
 // Middleware de manejo de errores
 app.use(errorHandler);
 
-// Middleware para autentificación
-app.use(authMiddleware);
+// Ruta de prueba
+app.get('/', (_req, res) => {
+  res.send('HTTPS!');
+});
 
-
-// // Iniciar el servidor
-// app.listen(port, () => {
-//   console.log(`Servidor corriendo en http://localhost:${port}`);
-// });
-
-// Iniciar el servidor HTTP en local
+// Iniciar el servidor
 if (process.env.NODE_ENV !== 'production') {
   app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
   });
 } else {
-  // Iniciar el servidor HTTPS en producción
-  const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/flamingoapi.integrador.xyz/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/flamingoapi.integrador.xyz/fullchain.pem')
-  };
+  try {
+    const options = {
+      key: fs.readFileSync('/etc/letsencrypt/live/flamingoapi.integrador.xyz/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/flamingoapi.integrador.xyz/fullchain.pem')
+    };
 
-  https.createServer(options, app).listen(port, () => {
-    console.log(`Servidor HTTPS corriendo en el puerto ${port}`);
-  });
+    https.createServer(options, app).listen(port, () => {
+      console.log(`Servidor HTTPS corriendo en el puerto ${port}`);
+    });
+  } catch (error) {
+    console.error('Error al leer los certificados SSL:', error);
+    process.exit(1);
+  }
 }
-
-// Ruta de prueba
-app.get('/', (_req, res) => {
-  res.send('HTTPS!');
-});
